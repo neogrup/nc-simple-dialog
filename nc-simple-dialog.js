1bffb671eb9270991ff0e471961a95eccd970e19
Marcel Miguel <marcel.miguel@gmail.com>
2026-01-22 16:24:21 +0100

    Fix subttitle

--- nc-simple-dialog.js
+++ nc-simple-dialog.js
@@ -115,7 +115,7 @@ class NcSimpleDialog extends mixinBehaviors([AppLocalizeBehavior], PolymerElemen
         <div class="header">
           <iron-icon icon="{{dialogIcon}}"></iron-icon><h3>{{localize(dialogTitle)}}</h3>
           <template is="dom-if" if="{{dialogSubTitle1}}">
-            <h1>{{localize(dialogSubTitle1)}}</h1>
+            <br/><p>{{localize(dialogSubTitle1)}}</p>
           </template>
         </div>
         <div class="content">
